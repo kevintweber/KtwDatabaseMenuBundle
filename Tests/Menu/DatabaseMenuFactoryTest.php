ab7d11e94de43688a62065c254a6020875099dd2
Kevin Weber <kevin.weber@site-kitchen.com>
2013-06-20 21:18:11 -0400

    Added provider tests.

--- Tests/Menu/DatabaseMenuFactoryTest.php
+++ Tests/Menu/DatabaseMenuFactoryTest.php
@@ -10,7 +10,8 @@ use kevintweber\KtwDatabaseMenuBundle\Menu\DatabaseMenuFactory;
  *
  * Since kevintweber\KtwDatabaseMenuBundle\Menu\DatabaseMenuFactory inherits
  * from Knp\Menu\Silex\RouterAwareFactory, I have copied many of the tests
- * from /Knp/Menu/Tests/MenuFactoryTest.php to here. Therefore most of
+ * from /Knp/Menu/Tests/MenuFactoryTest.php and
+ * Knp\Menu\Tests\Silex\RouterAwareFactoryTest.php to here. Therefore most of
  * these tests are thanks to stof of KNP Labs.  Thank you.
  */
 class DatabaseMenuFactoryTest extends \PHPUnit_Framework_TestCase
@@ -94,5 +95,87 @@ class DatabaseMenuFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(2, $item);
         $this->assertTrue(isset($item['john']));
         $this->assertTrue(isset($item['jack']));
+    }
+
+    public function testCreateItemWithRoute()
+    {
+        $generatorMock = $this->getMock('Symfony\Component\Routing\Generator\UrlGeneratorInterface');
+        $generatorMock->expects($this->once())
+            ->method('generate')
+            ->with('homepage', array(), false)
+            ->will($this->returnValue('/foobar'))
+        ;
+
+
+        $containerInterfaceMock = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $containerInterfaceMock->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValue('kevintweber\KtwDatabaseMenuBundle\Entity\MenuItem'));
+
+        $factory = new DatabaseMenuFactory($generatorMock,
+                                           $containerInterfaceMock);
+
+        $item = $factory->createItem('test_item', array('uri' => '/hello', 'route' => 'homepage'));
+        $this->assertEquals('/foobar', $item->getUri());
+    }
+
+    public function testCreateItemWithRouteAndParameters()
+    {
+        $generatorMock = $this->getMock('Symfony\Component\Routing\Generator\UrlGeneratorInterface');
+        $generatorMock->expects($this->once())
+            ->method('generate')
+            ->with('homepage', array('id' => 12), false)
+            ->will($this->returnValue('/foobar'))
+        ;
+
+        $containerInterfaceMock = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $containerInterfaceMock->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValue('kevintweber\KtwDatabaseMenuBundle\Entity\MenuItem'));
+
+        $factory = new DatabaseMenuFactory($generatorMock,
+                                           $containerInterfaceMock);
+
+        $item = $factory->createItem('test_item', array('route' => 'homepage', 'routeParameters' => array('id' => 12)));
+        $this->assertEquals('/foobar', $item->getUri());
+    }
+
+    public function testCreateItemWithAbsoluteRoute()
+    {
+        $generatorMock = $this->getMock('Symfony\Component\Routing\Generator\UrlGeneratorInterface');
+        $generatorMock->expects($this->once())
+            ->method('generate')
+            ->with('homepage', array(), true)
+            ->will($this->returnValue('http://php.net'))
+        ;
+
+        $containerInterfaceMock = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $containerInterfaceMock->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValue('kevintweber\KtwDatabaseMenuBundle\Entity\MenuItem'));
+
+        $factory = new DatabaseMenuFactory($generatorMock,
+                                           $containerInterfaceMock);
+
+        $item = $factory->createItem('test_item', array('route' => 'homepage', 'routeAbsolute' => true));
+        $this->assertEquals('http://php.net', $item->getUri());
+    }
+
+    public function testCreateItemAppendsRouteUnderExtras()
+    {
+        $generatorMock = $this->getMock('Symfony\Component\Routing\Generator\UrlGeneratorInterface');
+        $containerInterfaceMock = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $containerInterfaceMock->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValue('kevintweber\KtwDatabaseMenuBundle\Entity\MenuItem'));
+
+        $factory = new DatabaseMenuFactory($generatorMock,
+                                           $containerInterfaceMock);
+
+        $item = $factory->createItem('test_item', array('route' => 'homepage'));
+        $this->assertEquals(array('homepage'), $item->getExtra('routes'));
+
+        $item = $factory->createItem('test_item', array('route' => 'homepage', 'extras' => array('routes' => array('other_page'))));
+        $this->assertContains('homepage', $item->getExtra('routes'));
     }
 }
