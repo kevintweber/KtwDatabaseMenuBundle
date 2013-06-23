d2aac17d7ece9bf07b76917593ace333df2cf310
Kevin Weber <kevin.weber@site-kitchen.com>
2013-06-22 21:37:23 -0400

    Added license notice to test files.

--- Tests/Menu/DatabaseMenuFactoryTest.php
+++ Tests/Menu/DatabaseMenuFactoryTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the KtwDatabaseMenuBundle package.
+ *
+ * (c) Kevin T. Weber <https://github.com/kevintweber/KtwDatabaseMenuBundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace kevintweber\KtwDatabaseMenuBundle\Tests\Menu;
 
 use kevintweber\KtwDatabaseMenuBundle\Entity\MenuItem;
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the KtwParameterBundle package.
+ * This file is part of the KtwDatabaseMenuBundle package.
  *
- * (c) Kevin T. Weber <https://github.com/kevintweber/KtwParameterBundle/>
+ * (c) Kevin T. Weber <https://github.com/kevintweber/KtwDatabaseMenuBundle/>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
