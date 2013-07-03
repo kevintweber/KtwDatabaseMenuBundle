ec7165f83d1a37d20ae7dc35c4b4bed8a31ff33e
Kevin Weber <kevin.weber@site-kitchen.com>
2013-07-02 22:18:07 -0400

    Added DI test.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@
   As more stable versions are released, I will change the stability
   requirements.
 * Renamed configuration option `menu_item_repository` to `menu_item_entity`
-* Removed deprecated tests.
+* Removed deprecated tests, and added a few simple tests.
 * @TODO Getting a deprecation notice in the Silex\Voter\RouteVoter.
 
 ## 0.1 (2013-06-22)
---
+++ Tests/DependencyInjection/KtwDatabaseMenuExtensionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the KtwDatabaseMenuBundle package.
+ *
+ * (c) Kevin T. Weber <https://github.com/kevintweber/KtwDatabaseMenuBundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace kevintweber\KtwDatabaseMenuBundle\Tests\DependencyInjection;
+
+use kevintweber\KtwDatabaseMenuBundle\DependencyInjection\KtwDatabaseMenuExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\DependencyInjection\Reference;
+
+class KtwDatabaseMenuExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefault()
+    {
+        $container = new ContainerBuilder();
+        $loader = new KtwDatabaseMenuExtension();
+        $loader->load(array(array()), $container);
+        $this->assertEquals('kevintweber\KtwDatabaseMenuBundle\Entity\MenuItem', $container->getParameter('ktw_database_menu.menu_item_entity'));
+        $this->assertFalse($container->getParameter('ktw_database_menu.preload_menus'));
+    }
+}
